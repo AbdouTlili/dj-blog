88e8ac0a1d11ce49d23cda6022fd7fe1569e78d7
zacnnnt <ha_tlili@esi.dz>
2020-03-23 12:05:12 +0100

    adding new view and linking it to the urlConfs

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
     context_object_name = 'blog_posts_list'
+
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
+    
