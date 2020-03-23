aa7b3a7a17c94debf9a09b805f81c2f41e0eeddf
zacnnnt <ha_tlili@esi.dz>
2020-03-23 20:27:12 +0100

    adding account operations views and urls for : login,logout,password changing.. +  the login templates and login_redirect_url in settings.py

--- dj_blog/urls.py
+++ dj_blog/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
+    path('accounts/',include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
+
 ]
