6fda6857d8e55323b2211780f2f0b8d67720d05e
zacnnnt <ha_tlili@esi.dz>
2020-03-23 22:08:29 +0100

    adding the new app (accounts) to the project for SignUp view

--- dj_blog/settings.py
+++ dj_blog/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     ## local apps
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- dj_blog/urls.py
+++ dj_blog/urls.py
@@ -19,6 +19,7 @@ from django.urls import include
 
 urlpatterns = [
     path('accounts/',include('django.contrib.auth.urls')),
+    path('accounts/',include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
 
