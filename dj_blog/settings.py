56cd521177cf4c6e81919d30b19c678f888e1680
zacnnnt <ha_tlili@esi.dz>
2020-03-24 23:04:42 +0100

    typo in settings

--- dj_blog/settings.py
+++ dj_blog/settings.py
@@ -128,5 +128,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
